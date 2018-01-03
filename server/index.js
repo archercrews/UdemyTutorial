335224295e0cd02c431cbe4ee4d76e4973d44c2f
Harrison <crewsha@gmail.com>
2018-01-02 22:49:04 -0500

    Finished Payment Backend

--- server/index.js
+++ server/index.js
@@ -27,5 +27,18 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if(process.env.NODE_ENV === 'production') {
+    // Make sure express will serve up production assets
+    // like main.js or main.css.
+    app.use(express.static('client/build'));
+
+    // Make sure express will serve up index.html file if it doesn't
+    // recognize the route.
+    const path = require('path');
+    app.get('*', (req, res ) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
